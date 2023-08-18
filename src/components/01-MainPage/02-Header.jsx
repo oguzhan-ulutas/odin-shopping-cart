2b0a2e08a073e3c324dcb0fcf30578e95ee76f08
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-08-18 18:26:37 +0300

    Pass Header test

--- src/components/01-MainPage/02-Header.jsx
+++ src/components/01-MainPage/02-Header.jsx
@@ -1,5 +1,15 @@
-const Header = ()=>{
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-head">Jewelery Hell</div>
+      <div className="header-container">
+        <div>Home</div>
+        <div>Store</div>
+        <div>About</div>
+        <img src="../img/shopping-cart.png" alt="Shopping cart" />
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default Header,
+export default Header;
